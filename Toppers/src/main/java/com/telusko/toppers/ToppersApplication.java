18b43d8506feb678a335e20e5d9fdccb0a2a49a1
Madan-Ramineni <madan.ramineni@gmail.com>
2018-06-10 21:14:02 +0530

    First commit

--- Toppers/src/main/java/com/telusko/toppers/ToppersApplication.java
+++ Toppers/src/main/java/com/telusko/toppers/ToppersApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToppersApplication {
+public class ToppersApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToppersApplication.class, args);
